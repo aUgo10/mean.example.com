7e61a57a28d91bb11407cb50ab4833f8b5344139
Anthony Ugo <anthonyugo@gmail.com>
2020-02-03 20:55:19 -0600

    Add location.hash detection for app navigation

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -116,12 +116,22 @@ var authApp = (function() {
 
   return {
     load: function(){
-      registrationForm();
-      postRequest('registrationForm', '/api/auth/register');
-      validate.registrationForm();
+
+      switch(window.location.hash){
+        case '#register':
+          registrationForm();
+          postRequest('registrationForm', '/api/auth/register');
+          validate.registrationForm();
+          break;
+
+        default:
+          loginForm();
+          postRequest('loginForm', '/api/auth/login');
+          break;
+      }
+
     }
   }
-
 })();
 var validate = (function() {
 
@@ -150,3 +160,6 @@ var validate = (function() {
 
 })();
 authApp.load();
+window.addEventListener("hashchange", function(){
+  authApp.load();
+});
